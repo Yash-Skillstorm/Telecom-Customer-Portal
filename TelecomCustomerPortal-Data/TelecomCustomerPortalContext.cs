256a78da43dcee787aec226ddfc1b672fe78f64d
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-10-19 17:00:28 -0500

    Added Web api with connection string from azure

---
+++ TelecomCustomerPortal-Data/TelecomCustomerPortalContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelecomCustomerPortal_Domain;
+
+namespace TelecomCustomerPortal_Data
+{
+    public class TelecomCustomerPortalContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<Device> Device { get; set; }
+        public DbSet<Plan> Plan { get; set; }
+        public TelecomCustomerPortalContext()
+        {
+
+        }
+        public TelecomCustomerPortalContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
