8d6381a1e90fe2841e85cd25a199c068e3cec671
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-10-26 15:47:18 -0500

    User model change

--- TelecomCustomerPortal.Domain/User.cs
+++ TelecomCustomerPortal.Domain/User.cs
@@ -8,7 +8,7 @@ namespace TelecomCustomerPortal.Domain
 {
     public class User
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }        
         //public List<Plan> Plan { get; set; }
